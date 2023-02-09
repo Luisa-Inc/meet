b6ec339c70d33914b890debd83b664899e332a04
Luisa Ihle <luisa.ihle@gmail.com>
2023-02-09 14:50:49 +0700

    update 4.4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./nprogress.css";
 // COMPONENTS //////////
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -7,6 +7,7 @@ class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
+    showSuggestions: undefined,
   };
 
   handleInputChanged = (event) => {
@@ -24,6 +25,7 @@ class CitySearch extends Component {
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      showSuggestions: false,
     });
 
     this.props.updateEvents(suggestion);
@@ -37,8 +39,14 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
         />
-        <ul className="suggestions">
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
