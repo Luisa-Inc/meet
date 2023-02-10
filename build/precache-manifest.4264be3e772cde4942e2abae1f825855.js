2374dc5ab42762b0d7711bd8b7e8465f2a54e0cd
Luisa Ihle <luisa.ihle@gmail.com>
2023-02-10 12:36:35 +0700

    update gh pages 4.4._10.02.

---
+++ build/precache-manifest.4264be3e772cde4942e2abae1f825855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4d190bee244c11225a",
+    "url": "/meet/static/css/main.9bdd23ad.chunk.css"
+  },
+  {
+    "revision": "ce4d190bee244c11225a",
+    "url": "/meet/static/js/main.b7168dda.chunk.js"
+  },
+  {
+    "revision": "3a7bd253ebe168702fdc",
+    "url": "/meet/static/js/runtime~main.f972cc4f.js"
+  },
+  {
+    "revision": "34d56192d2d5715e173a",
+    "url": "/meet/static/js/2.ede535e0.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/meet/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "b08107a4c4cdb1efab6ea430178edab8",
+    "url": "/meet/index.html"
+  }
+];
