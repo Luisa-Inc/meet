d41f6953bbaa8035a17576c92ccaad6d0a94d3fd
Luisa Ihle <luisa.ihle@gmail.com>
2023-02-10 12:07:41 +0700

    update 4.4_10.02.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-// VARS and FUNCS //////////
-import { mockData } from "./mock-data";
-import { extractLocations } from "./api";
 
 class CitySearch extends Component {
   state = {
@@ -12,36 +9,50 @@ class CitySearch extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    const locations = extractLocations(mockData);
-    const suggestions = locations.filter((location) => {
+    this.setState({ showSuggestions: true });
+    const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        suggestions: [],
+        showSuggestions: false,
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions: suggestions,
+      });
+    }
   };
 
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      suggestions: [],
       showSuggestions: false,
     });
 
     this.props.updateEvents(suggestion);
   };
 
+  handleInputFocus = () => {
+    this.setState({ showSuggestions: true });
+  };
+
   render() {
     return (
       <div className="CitySearch">
+        <h3>Find a City</h3>
         <input
           type="text"
           className="city"
+          placeholder="Search"
           value={this.state.query}
           onChange={this.handleInputChanged}
-          onFocus={() => {
-            this.setState({ showSuggestions: true });
-          }}
+          onFocus={this.handleInputFocus}
         />
         <ul
           className="suggestions"
@@ -55,8 +66,8 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li onClick={() => this.handleItemClicked("all")}>
-            <b>See all cities</b>
+          <li key="all" onClick={() => this.handleItemClicked("all")}>
+            <h4>See all cities</h4>
           </li>
         </ul>
       </div>
