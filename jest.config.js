10af59379f3f26fe04aeb074f9e7466ef36ac84c
Luisa Ihle <luisa.ihle@gmail.com>
2023-05-06 19:27:54 +0200

    create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  transformIgnorePatterns: ["node_modules/(?!(axios|nprogress)/)"],
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+};
