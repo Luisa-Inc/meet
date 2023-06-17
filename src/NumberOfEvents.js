bdbab5ba4ff51a942bbd762881d1c9aa2735ab8b
Luisa Ihle <luisa.ihle@gmail.com>
2023-06-17 14:38:23 +0200

    update 4.8_set 2 alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,32 +1,44 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
-  static defaultProps = {
-    numberOfResults: 32,
-  };
+  constructor() {
+    super();
+    this.state = {
+      query: 32,
+      errorText: "",
+    };
+  }
 
-  state = {
-    numberOfEvents: this.props.numberOfResults,
-  };
-
-  handleInputChange = (event) => {
-    const value = parseInt(event.target.value);
-    this.setState({ numberOfEvents: value });
-    //this.props.updateNumberOfResults(value); // Call the updateNumberOfResults function passed from App.js
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value >= 1 || value <= 32) {
+      this.setState({
+        query: value,
+        errorText: "",
+      });
+      this.props.updateEvents(this.props.selectedCity, value);
+    }
+    if (value < 1 || value > 32) {
+      this.setState({
+        query: value,
+        errorText: "Please enter a valid number",
+      });
+    }
   };
 
   render() {
     return (
-      <div className="number-of-events">
-        <label htmlFor="event-number-input">Number of Events: </label>
+      <div className="numberOfEvents">
         <input
           type="number"
-          className="event-number-input"
-          id="event-number-input"
-          value={this.state.numberOfEvents}
-          onChange={this.handleInputChange}
-          placeholder="Enter number of events"
+          className="numberOfEvents"
+          min={1}
+          max={32}
+          value={this.state.query}
+          onChange={this.handleInputChanged}
         />
+        <ErrorAlert className="errorMessage" text={this.state.errorText} />
       </div>
     );
   }
