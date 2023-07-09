5fc21a4b7f2a4de19aab34919a6e48c711917a7c
Luisa Ihle <luisa.ihle@gmail.com>
2023-07-09 08:06:24 +0200

    Update NumberOfEvents.js
    fixed display of max 32 events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -12,14 +12,14 @@ class NumberOfEvents extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    if (value >= 1 || value <= 32) {
+    if (value > 0 && value <= 32) {
       this.setState({
         query: value,
         errorText: "",
       });
       this.props.updateEvents(this.props.selectedCity, value);
     }
-    if (value < 1 || value > 32) {
+    if (value > 0 && value > 32) {
       this.setState({
         query: value,
         errorText: "Please enter a valid number",
